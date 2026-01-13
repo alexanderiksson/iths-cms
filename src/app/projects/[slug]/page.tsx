e6af5c1b5a3f2390e3889d8148cef72adff81f4c
alexanderiksson <Alexerik69@gmail.com>
2026-01-13 15:41:54 +0100

    Fixes

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,24 +1,40 @@
 import client from "@/lib/contentful";
+import { notFound } from "next/navigation";
+import Image from "next/image";
+import { Asset } from "contentful";
 
 interface PageProps {
     params: Promise<{ slug: string }>;
 }
 
-export default async function Page({ params }: PageProps) {
+export default async function ProjectPage({ params }: PageProps) {
     const { slug } = await params;
 
-    const data = await client.getEntries({
+    const entry = await client.getEntries({
         content_type: "portfolio",
         "fields.slug": slug,
         limit: 1,
     });
 
-    const project = data.items[0].fields;
+    const project = entry.items[0];
+
+    if (!project) return notFound();
+
+    const img = project.fields.image as Asset;
 
     return (
         <div className="content">
-            <h1 className="heading">{project.title as string}</h1>
-            <p>{project.description as string}</p>
+            <h1 className="heading">{project.fields.title as string}</h1>
+            <p>{project.fields.description as string}</p>
+
+            <div className="relative w-full overflow-hidden aspect-4/3">
+                <Image
+                    src={`https:${img.fields.file?.url}`}
+                    alt={img.fields.title as string}
+                    fill
+                    className="object-contain"
+                />
+            </div>
         </div>
     );
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -8,29 +8,38 @@ export const metadata: Metadata = {
 };
 
 export default async function Projects() {
-    const projects = await client.getEntries({
+    const entries = await client.getEntries({
         content_type: "portfolio",
         order: ["-sys.createdAt"],
     });
 
+    const projects = entries.items;
+
     return (
         <div className="content">
             <h1 className="heading">Projects</h1>
-            <div className="flex flex-col gap-8">
-                {projects.items.map((project, i) => {
-                    const img = project.fields.image as Asset;
 
-                    return (
-                        <ProjectCard
-                            key={i}
-                            slug={project.fields.slug as string}
-                            title={project.fields.title as string}
-                            description={project.fields.description as string}
-                            img={img.fields.file?.url as string}
-                            imgAlt={img.fields.title as string}
-                        />
-                    );
-                })}
+            <div className="flex flex-col gap-8">
+                {projects.length <= 0 ? (
+                    <p>No projects found</p>
+                ) : (
+                    projects.map((project, i) => {
+                        const img = project.fields.image as Asset;
+
+                        return (
+                            <ProjectCard
+                                key={i}
+                                slug={project.fields.slug as string}
+                                title={project.fields.title as string}
+                                description={
+                                    project.fields.description as string
+                                }
+                                img={img.fields.file?.url as string}
+                                imgAlt={img.fields.title as string}
+                            />
+                        );
+                    })
+                )}
             </div>
         </div>
     );
